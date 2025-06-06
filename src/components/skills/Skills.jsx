d6954febdb7839f473f6995b8a7c077fc3b822da
Project-Nipon <oakee28@gmail.com>
2025-06-06 22:25:34 +0700

    initial commit

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './Skills.module.css'
-import { FaSquareJs, FaReact, FaSass, FaGitAlt, FaNode } from 'react-icons/fa6'
+import { FaSquareJs, FaReact, FaSass, FaGitAlt, FaNode, FaPython, FaGithub   } from 'react-icons/fa6'
+import { TiHtml5 } from "react-icons/ti";
+import { DiCss3 } from "react-icons/di";
+import { SiFastapi, SiSqlite } from "react-icons/si";
+import { BsFiletypeSql } from "react-icons/bs";
 
 function Skills() {
   return (
@@ -8,19 +12,34 @@ function Skills() {
         <h3 className={styles.skills_title}>My Skill Set</h3>
         <ul className={styles.skills_list}>
             <li>
+                <TiHtml5 />
+            </li>
+            <li>
+                <DiCss3 />
+            </li>
+            <li>
                 <FaSquareJs />
             </li>
             <li>
                 <FaReact />
             </li>
             <li>
-                <FaSass />
-            </li>
-            <li>
-                <FaGitAlt />
-            </li>
-            <li>
                 <FaNode />
+            </li>
+            <li>
+                <FaGithub />
+            </li>
+            <li>
+                <FaPython />
+            </li>
+            <li>
+                <SiFastapi />
+            </li>
+            <li>
+                <SiSqlite />
+            </li>
+            <li>
+                <BsFiletypeSql />
             </li>
         </ul>
     </div>
